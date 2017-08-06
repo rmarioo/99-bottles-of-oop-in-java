803942e036bf0f700b28de54574d6d1dd01d5bbc
Mario Russo <mario.russo@lastminute.com>
2017-08-06 17:20:11 +0200

    making case 2 and 3 identical. Now the second row is identical

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -14,13 +14,13 @@ public class Bottles
     if (number == 3)
       return
         number +" bottles of beer on the wall, " +
-        "3 bottles of beer.\n" +
+        number +" bottles of beer.\n" +
         "Take one down and pass it around, " +
         "2 bottles of beer on the wall.\n";
     else
     return
         number +" bottles of beer on the wall, " +
-        "99 bottles of beer.\n" +
+        number +" bottles of beer.\n" +
         "Take one down and pass it around, " +
         "98 bottles of beer on the wall.\n";
   }
