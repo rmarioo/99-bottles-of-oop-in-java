f745b201a2aaf945a9474b7deb5b5c41e93d3250
Mario Russo <mario.russo@lastminute.com>
2017-08-06 17:15:25 +0200

    first verse test

---
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -0,0 +1,9 @@
+package com.rmarioo.bottle99;
+
+public class Bottles
+{
+  public String verse(int i)
+  {
+    return null;
+  }
+}
--- src/test/java/com/rmarioo/bottle99/BottleTest.java
+++ src/test/java/com/rmarioo/bottle99/BottleTest.java
@@ -1,6 +1,24 @@
 package com.rmarioo.bottle99;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class BottleTest
 {
+
+  private final Bottles bottles = new Bottles();
+
+  @Test
+  public void firstVerse() throws Exception
+  {
+    String result = bottles.verse(99);
+    assertThat(result,is(
+        "99 bottles of beer on the wall, " +
+        "99 bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "98 bottles of beer on the wall.\n"));
+  }
 
 }
