dd3261463f669927c25d7c41b723c84f5ad0dbf3
Mario Russo <mario.russo@lastminute.com>
2017-08-06 17:25:32 +0200

    identified case 2 and last case as most identical cases. Start using flocking rules to make them identical Now the first row of those cases is identical

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -20,7 +20,7 @@ public class Bottles
     else
     if (number == 2)
       return
-        "2 bottles of beer on the wall, " +
+        number +" bottles of beer on the wall, " +
         "2 bottles of beer.\n" +
         "Take one down and pass it around, " +
         "1 bottle of beer on the wall.\n";
