2491a15bfbde1c0116e1920fd840b9346429b60c
Mario Russo <mario.russo@lastminute.com>
2017-08-06 17:18:55 +0200

    formatting to have a similar look in all cases

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -19,9 +19,9 @@ public class Bottles
         "2 bottles of beer on the wall.\n";
     else
     return
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n";
+        "99 bottles of beer on the wall, " +
+        "99 bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "98 bottles of beer on the wall.\n";
   }
 }
