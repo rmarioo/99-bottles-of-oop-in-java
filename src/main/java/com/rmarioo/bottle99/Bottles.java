24ce22e43b9171c773799061dcd7ed20f457c211
Mario Russo <mario.russo@lastminute.com>
2017-08-06 18:21:29 +0200

    working on last row

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -9,7 +9,7 @@ public class Bottles
        capitalize(quantity(number)) +" " + container(number) +" of beer on the wall, " +
        quantity(number) +" " + container(number) +" of beer.\n" +
        action(number) + ", " +
-       "99 bottles of beer on the wall.\n";
+       "99 " + container(number-1) + " of beer on the wall.\n";
     else
     return
        capitalize(quantity(number)) +" " + container(number) +" of beer on the wall, " +
