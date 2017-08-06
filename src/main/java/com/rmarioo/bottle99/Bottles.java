ea86fb444ab0e63b30ea2f5751b95413f6eb174c
Mario Russo <mario.russo@lastminute.com>
2017-08-06 17:41:03 +0200

    looking at last two case as most similar. The second row is now identical

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -14,7 +14,7 @@ public class Bottles
     if (number ==1)
       return
         number +" bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
+        number +" bottle of beer.\n" +
         "Take it down and pass it around, " +
         "no more bottles of beer on the wall.\n";
     else
