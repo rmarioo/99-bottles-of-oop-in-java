b0e33595f8677eb5d9afe60583ad1a6319dec0c4
Mario Russo <mario.russo@lastminute.com>
2017-08-06 18:16:35 +0200

    using capitalize in last case

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -12,7 +12,7 @@ public class Bottles
        "99 bottles of beer on the wall.\n";
     else
     return
-        quantity(number) +" " + container(number) +" of beer on the wall, " +
+        capitalize(quantity(number)) +" " + container(number) +" of beer on the wall, " +
         quantity(number) +" " + container(number) +" of beer.\n" +
         "Take "+ pronoun(number)+" down and pass it around, " +
         quantity(number-1) +" " + container(number-1) + " of beer on the wall.\n";
