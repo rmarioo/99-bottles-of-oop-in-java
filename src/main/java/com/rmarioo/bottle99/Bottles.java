ad5d0edeaa2392a1bb2b4dc2f4e2c73b1c2ef592
Mario Russo <mario.russo@lastminute.com>
2017-08-06 17:40:39 +0200

    looking at last two case as most similar. The first row is not identical

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -13,7 +13,7 @@ public class Bottles
     else
     if (number ==1)
       return
-        "1 bottle of beer on the wall, " +
+        number +" bottle of beer on the wall, " +
         "1 bottle of beer.\n" +
         "Take it down and pass it around, " +
         "no more bottles of beer on the wall.\n";
