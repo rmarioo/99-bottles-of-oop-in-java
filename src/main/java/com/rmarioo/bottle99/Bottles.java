f0baa78dcc590e4acfa2a104e7ec37b995468279
Mario Russo <mario.russo@lastminute.com>
2017-08-06 17:29:47 +0200

    The method "container" it now is used in the last case

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -29,7 +29,7 @@ public class Bottles
         number +" bottles of beer on the wall, " +
         number +" bottles of beer.\n" +
         "Take one down and pass it around, " +
-        (number-1) +" bottles of beer on the wall.\n";
+        (number-1) +" " + container() + " of beer on the wall.\n";
   }
 
   public String container()
