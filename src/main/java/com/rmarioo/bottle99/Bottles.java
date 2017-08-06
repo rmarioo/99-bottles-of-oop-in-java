6093444dab164b33d9855039659b40ff4a63b097
Mario Russo <mario.russo@lastminute.com>
2017-08-06 17:15:43 +0200

    tirvial fix

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -4,6 +4,10 @@ public class Bottles
 {
   public String verse(int i)
   {
-    return null;
+    return
+      "99 bottles of beer on the wall, " +
+      "99 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of beer on the wall.\n";
   }
 }
