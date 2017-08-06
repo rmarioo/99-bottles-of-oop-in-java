c8683a89b53ca2ae20e591795ae1d73beb2e5965
Mario Russo <mario.russo@lastminute.com>
2017-08-06 17:59:35 +0200

    removed duplicated case

--- src/main/java/com/rmarioo/bottle99/Bottles.java
+++ src/main/java/com/rmarioo/bottle99/Bottles.java
@@ -11,13 +11,6 @@ public class Bottles
        "Go to the store and buy some more, " +
        "99 bottles of beer on the wall.\n";
     else
-    if (number ==1)
-      return
-        quantity(number) +" " + container(number) +" of beer on the wall, " +
-        quantity(number) +" " + container(number) +" of beer.\n" +
-        "Take "+ pronoun(number)+" down and pass it around, " +
-        quantity(number-1) +" " + container(number-1) + " of beer on the wall.\n";
-    else
     return
         quantity(number) +" " + container(number) +" of beer on the wall, " +
         quantity(number) +" " + container(number) +" of beer.\n" +
